155511a42ae67fc6c3e32b7ec192a8170b93949e
Neyoraa <chinenyeorakanma@gmail.com>
2023-08-25 21:30:21 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,7 +10,8 @@ void rev_string(char *n)
 	int j = 0;
 	char temp;
 	while (*(n + i) != '\0')
-	i++;
+	{
+		i++;
 	}
 	i--;
 	for (j = 0; j < i; j++, i--)
@@ -30,10 +31,12 @@ void rev_string(char *n)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 	while (*(n1 + i) != '\0')
 		i++;
 	while (*(n2 + j) != '\0')
-	j++;
+		j++;
 	i--;
 	j--;
 	if (j >= size_r || i >= size_r)
@@ -55,7 +58,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			overflow = 0;
 		if (digits >= (size_r - 1))
 		return (0);
-	*(r + digits) = (temp_tot % 10) + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
